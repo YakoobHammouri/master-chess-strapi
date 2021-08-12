d42051bcd7e8a533b69a407c83f6fed243029772
YakoobHammouri <jakoub-h2002@hotmail.com>
2021-08-12 10:36:42 +0300

    update comp index
    Signed-off-by: YakoobHammouri <jakoub-h2002@hotmail.com>

--- plugins/master-chess/admin/src/components/index.js
+++ plugins/master-chess/admin/src/components/index.js
@@ -1,15 +1,7 @@
 import Label from "./Label";
 import LoadingProgress from "./LoadingProgress";
 import Dropdowns from "./Dropdowns";
-import CoursesList from "./CoursesList";
 import AttendanceList from "./AttendanceList";
 import DatePicker from "./DatePicker";
 
-export {
-  LoadingProgress,
-  Label,
-  Dropdowns,
-  CoursesList,
-  DatePicker,
-  AttendanceList,
-};
+export { LoadingProgress, Label, Dropdowns, DatePicker, AttendanceList };
