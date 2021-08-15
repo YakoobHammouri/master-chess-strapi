d9c3b41a48c61b5834e5c73e10b23f7e157a3d92
YakoobHammouri <jakoub-h2002@hotmail.com>
2021-08-15 22:29:53 +0300

    set Std Name
    Signed-off-by: YakoobHammouri <jakoub-h2002@hotmail.com>

--- api/attendance/models/attendance.js
+++ api/attendance/models/attendance.js
@@ -5,15 +5,51 @@
  * to customize this model
  */
 
+let isLoack = false;
+let needUpdate = false;
 module.exports = {
   lifecycles: {
     async afterCreate(result, data) {
-      console.log("daatrtsat : ", data);
-      console.log("result : ", result);
+      try {
+        result?.studentAttendance?.forEach((std) => {
+          if (!std.studentName) {
+            console.log("std 111 :  ", std);
+            std.studentName = std.student.name;
+            isLoack = true;
+            needUpdate = true;
+          }
+        });
+
+        if (needUpdate) {
+          await strapi.query("attendance").update({ id: result.id }, result);
+          isLoack = false;
+        }
+      } catch (err) {
+        console.log("error in atteddfdf 12124", err);
+      }
     },
-    async afterUpdate(result, data) {
-      console.log("daatrtsat afterUpdate : ", data);
-      console.log("result  afterUpdate: ", result);
+    async afterUpdate(result, params, data) {
+      try {
+        // locl code to update
+        if (isLoack === false) {
+          console.log(" isLoack 1111111111 : ", isLoack);
+          result?.studentAttendance?.forEach((std) => {
+            if (!std.studentName) {
+              isLoack = true;
+              needUpdate = true;
+              std.studentName = std.student.name;
+            }
+          });
+
+          if (needUpdate) {
+            await strapi.query("attendance").update({ id: result.id }, result);
+            isLoack = false;
+            needUpdate = false;
+          }
+        }
+      } catch (err) {
+        console.log("error in atteddfdf 12124", err);
+      }
     },
   },
 };
